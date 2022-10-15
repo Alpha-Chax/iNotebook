ac6b3163d227d94991be98f8996b8792dd2a99e7
DizzyShadoWWW <73545057+DizzyShadoWWW@users.noreply.github.com>
2022-10-15 16:37:01 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './Components/Home'
+import About from './Components/About'
+import Navbar from './Components/Navbar'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook.</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar /> 
+      <div className="container">
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About/>
+        </Route> 
+      </Switch>
+      </div>
+    </Router>
+  </>
   )
 }
 
