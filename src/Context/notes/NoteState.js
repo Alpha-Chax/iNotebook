6248700d70b8452bf2e1b6a6453f8ba6e9682bc7
DizzyShadoWWW <73545057+DizzyShadoWWW@users.noreply.github.com>
2022-10-16 07:52:21 +0530

    Bug Fixed

--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -17,7 +17,6 @@ const NoteState = (props) => {
       }
     });
     const json = await response.json()
-    console.log(json)
     setNotes(json)
   }
 
@@ -36,19 +35,8 @@ const NoteState = (props) => {
         body: JSON.stringify({title, description, tag}),
       }
     );
-   const json = response.json();
-   console.log(json);
-    
-    const note = {
-      _id: "61c322f19553781a8ca8d0e08",
-      user: "6131dc5e3e4037cd4734a066",
-      title: title,
-      description: description,
-      tag: tag,
-      date: "2021-09-03T14:20:09.668Z",
-      __v: 0,
-    };
-    setNotes(notes.concat(note));
+   const note = await response.json();
+   setNotes(notes.concat(note));
   };
   //Delete a Note
   const deleteNote = async(id) => {
@@ -65,9 +53,7 @@ const NoteState = (props) => {
       }
     );
    const json = response.json();
-   console.log(json);
 
-    console.log("Deleting note with id" + id);
     const newNotes = notes.filter((note) => {
       return note._id !== id;
     });
@@ -89,7 +75,7 @@ const NoteState = (props) => {
       }
     );
    const json = await response.json();
-   console.log(json);
+  
     
    let newNotes = JSON.parse(JSON.stringify(notes))
     // Logic to edit in client
