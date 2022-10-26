8ef86431a0542d6b7948a0e669e019e312dc7026
DizzyShadoWWW <73545057+DizzyShadoWWW@users.noreply.github.com>
2022-10-26 15:22:41 +0530

    index

--- Backend/index.js
+++ Backend/index.js
@@ -18,14 +18,12 @@ app.use('/api/notes', require('./routes/notes'))
 __dirname = path.resolve();
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join('build', 'index.html'));
-  });
+  app.get("*", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "Client", "build", "index.html"))
+  })
 }
 
-app.get("*", (req,res) =>{
-  res.sendFile(path.resolve(__dirname, "Client", "build", "index.html"))
-})
+
 
 //--------------DEVELOPMENT-----------------------------
 
