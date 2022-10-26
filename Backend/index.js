87d2bae3dda3fe89c944146f40c97e44b1fbf7b7
DizzyShadoWWW <73545057+DizzyShadoWWW@users.noreply.github.com>
2022-10-26 21:53:56 +0530

    index

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use('/api/notes', require('./routes/notes'))
 //--------------DEVELOPMENT----------------------------
 __dirname = path.resolve();
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('build'));
+  app.use(express.static(path.join(__dirname,'/Client/build')));
   app.get("*", (req,res) =>{
     res.sendFile(path.resolve(__dirname, "Client", "build", "index.html"))
   })
